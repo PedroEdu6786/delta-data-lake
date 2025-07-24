3d0f5b2728dde98bac34ece82b8423a4eb734969
Pedro Cruz <pedrocruz@drata.com>
2025-07-24 15:30:08 -0600

    feat: remove unused package

--- microservice-b/src/app.module.ts
+++ microservice-b/src/app.module.ts
@@ -3,22 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PermissionsController } from './permissions/permissions.controller';
 import { typeOrmConfig } from './commons/configs/typeorm.config';
-import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { PermissionsModule } from './permissions/permissions.module';
 import { LocalAuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true, // So it's available app-wide
-      envFilePath: '.env', // Optional if your file is named `.env`
-    }),
-    typeOrmConfig,
-    UsersModule,
-    PermissionsModule,
-    LocalAuthModule,
-  ],
+  imports: [typeOrmConfig, UsersModule, PermissionsModule, LocalAuthModule],
   controllers: [AppController, PermissionsController],
   providers: [AppService],
 })
